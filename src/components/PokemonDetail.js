16e7085bc05e3da013724ccf9e54cdc64591f595
Viviana Gomez <vivianakgp@gmail.com>
2022-04-02 06:14:16 -0600

    pokemon detail structure

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import {backgroundAccordingToType} from '../utilities/cardsBackground';
 import Header from './Header';
 
 function PokemonDetail() {
@@ -15,20 +16,34 @@ function PokemonDetail() {
             })
     },[ name ])
     // <h2>{pokemonDetail.name}</h2>
+    const pokemonType = pokemonDetail.types?.[0].type.name;
 
     return (
-    <div className='PokemonDetail'>
+    <div className='pokemonDetail'>
         <Header />
-        {/* <div className='content'>
-            <div className='baner'>
+        <div className='content'>
+            <div className='banner' style={{background:backgroundAccordingToType(pokemonType)}}>
                 <div className='containerImage'>
-                    <div><img src={} alt=''/></div>
+                    <div><img src={pokemonDetail.sprites?.front_default} alt=''/></div>
                 </div>
             </div>
-            <section className='sec mainInfo'></section>
+            <section className='mainInfo'>
+                <p>{pokemonDetail.id}</p>
+                <h1>{pokemonDetail.name}</h1>
+                <div>
+                    <sapn>
+                        <p>Peso</p>
+                        <sapn>{pokemonDetail.height}</sapn>
+                    </sapn>
+                    <sapn>
+                        <p>Altura</p>
+                        <sapn>{pokemonDetail.weight}</sapn>
+                    </sapn>
+                </div>
+            </section>
             <section className='sec stats'></section>
             <section  className='sec moments'></section>
-        </div> */}
+        </div>
     </div>
     );
 }
