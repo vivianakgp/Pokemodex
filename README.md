2ff30b8848f85f6e9cd77f5b60555b83e0b05fbb
viviana <vivianakgp@gmail.com>
2022-05-13 10:49:43 -0500

    dreadme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # POKEDEX
+
+React web app renders list of pokemon in the form of info cards and filters by pokemon name and type.
+
+## Description
+
+===
+Pokedex shows a card list of more than 1000 pokemons with images and basic description.
+...
+Each card contains the pokemon images, name and type. It also contains the number of attak, defense and spead. Additionally the background change acordding to the type of pokemon.
+...
+Each Pokemon card has a route of detail page path, here it is described in more detail about it.
+...
+You can search a pokemon by name or filter them by pokemon type.
+...
+
+### Technologies
+
+===
+
+- Create React App
+- React Router
+- Redux
+- React Hooks
+- Responsive Web Design
+- sass
+
+#### Installation
+
+===
+To run on your local computer
+
+- git fork
+- git clone
+- npm install
+- npm start
+  you can also see a demo [here](https://pokemonsdex.netlify.app/)
--- src/components/PokemonCards.js
+++ src/components/PokemonCards.js
@@ -1,59 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { backgroundAccordingToType, colorAccordingToType } from '../utilities/cardsBackground';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import {
+  backgroundAccordingToType,
+  colorAccordingToType,
+} from "../utilities/cardsBackground";
 
-function PokemonCards({pokeUrl}) {
-    const [ pokemon, setPokemon ] = useState({});
-    useEffect(()=>{
-        axios.get(pokeUrl).then(res => {
-                // console.log(res.data);
-                setPokemon(res.data);
-            })
-    },[ pokeUrl ]);
-    const pokemonType = pokemon.types?.[0]?.type.name;
-    const pokemonType2 = pokemon.types?.[1]?.type.name;
-    const shortName = () => {
-        if(pokemon.name?.length <= 11) {
-            return '.8em'
-        } else {
-            return'.6em'
-        }
+function PokemonCards({ pokeUrl }) {
+  const [pokemon, setPokemon] = useState({});
+  useEffect(() => {
+    axios.get(pokeUrl).then((res) => {
+      console.log(res.data);
+      setPokemon(res.data);
+    });
+  }, [pokeUrl]);
+  const pokemonType = pokemon.types?.[0]?.type.name;
+  const pokemonType2 = pokemon.types?.[1]?.type.name;
+  const shortName = () => {
+    if (pokemon.name?.length <= 11) {
+      return ".8em";
+    } else {
+      return ".6em";
     }
-    return (
-    <div className='PokemonCard' style={{background:backgroundAccordingToType(pokemonType)}}>
-        <Link style={{textDecoration: 'none'}} to={`/pokedex/${pokemon.name}`} >
-            <div className='containerImg' >
-                <img src={pokemon.sprites?.front_default} alt='pokemon'/>
-            </div>
-            <div className='containerInfo'  style={{color:colorAccordingToType(pokemonType)}} >
-                <h2 style={{fontSize:shortName()}}>{pokemon.name}</h2>
-                <h3>{pokemonType}/{pokemonType2}</h3>
-                <p>TIPO</p>
-                <section className='secStats borderTop'>
-                    <span>
-                        <span>HP</span>
-                        <h4>{pokemon.stats?.[0].base_stat}</h4>
-                    </span>
-                    <span>
-                        <span>ATAQUE</span>
-                        <h4>{pokemon.stats?.[1].base_stat}</h4>
-                    </span>
-                </section>
-                <section className='secStats'>
-                    <span>
-                        <span>DEFENSA</span>
-                        <h4>{pokemon.stats?.[2].base_stat}</h4>
-                    </span>
-                    <span>
-                        <span>VELOCIDAD</span>
-                        <h4>{pokemon?.stats?.[5].base_stat}</h4>
-                    </span>
-                </section>
-            </div>
-        </Link>
+  };
+  return (
+    <div
+      className="PokemonCard"
+      style={{ background: backgroundAccordingToType(pokemonType) }}
+    >
+      <Link style={{ textDecoration: "none" }} to={`/pokedex/${pokemon.name}`}>
+        <div className="containerImg">
+          <img src={pokemon.sprites?.front_default} alt="pokemon" />
+        </div>
+        <div
+          className="containerInfo"
+          style={{ color: colorAccordingToType(pokemonType) }}
+        >
+          <h2 style={{ fontSize: shortName() }}>{pokemon.name}</h2>
+          <h3>
+            {pokemonType}/{pokemonType2}
+          </h3>
+          <p>TIPO</p>
+          <section className="secStats borderTop">
+            <span>
+              <span>HP</span>
+              <h4>{pokemon.stats?.[0].base_stat}</h4>
+            </span>
+            <span>
+              <span>ATAQUE</span>
+              <h4>{pokemon.stats?.[1].base_stat}</h4>
+            </span>
+          </section>
+          <section className="secStats">
+            <span>
+              <span>DEFENSA</span>
+              <h4>{pokemon.stats?.[2].base_stat}</h4>
+            </span>
+            <span>
+              <span>VELOCIDAD</span>
+              <h4>{pokemon?.stats?.[5].base_stat}</h4>
+            </span>
+          </section>
+        </div>
+      </Link>
     </div>
-    );
+  );
 }
 
 export default PokemonCards;
